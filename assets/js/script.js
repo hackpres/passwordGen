1712132ea28912d5b4b6f1ae4acc9e4d6399e544
hackpres <102390515+hackpres@users.noreply.github.com>
2022-05-11 11:53:33 -0400

    created const variables to hold the various character options in arrays, separated between lowercase, uppercase, numbers, and special characters.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,13 @@ function generatePassword() {
   while (isNaN(promptLength) || promptLength < 8 || promptLength > 128) {
     promptLength = prompt("Please type a password length between 8 - 128.");
   }
+
+  const lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+  const upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+  const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const specialChars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "'", "|", ";", ":", ",", "<", ".", ">", "/", "?"];
+
+  
 }
 // Write password to the #password input
 function writePassword() {
